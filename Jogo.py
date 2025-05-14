628a13fc1d18625797ade77681e334b9d3f2f99b
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-14 14:48:46 -0300

    "Primeira versão" do jogo criada

--- Jogo.py
+++ Jogo.py
@@ -1,1 +1,24 @@
+import pygame
+import random
+from configuracoes import *
 from funcoes import *
+from janela import *
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('PyRacing')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = janela(window)
+    elif state == GAME:
+        state = janela(window)
+    else:
+        state =janela(window)
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
