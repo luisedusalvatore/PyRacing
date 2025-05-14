a77bb4f489e3152711d159f7dcbf7e9f86a327a4
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-14 15:48:18 -0300

    Tela principal do jogo iniciada

--- game_screen.py
+++ game_screen.py
@@ -1,2 +1,21 @@
+import pygame
+from configuracoes import *
+import os
+from classes import *
+from assets import *
 def game_screen(window):
-    print("hello world")
+    # Relógio do sistema
+    clock = pygame.time.Clock()
+    background = assets[fundo]
+    player = Piloto (groups, assets)
+
+    state = True
+    while state != False:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = False
+        window.fill(BLACK)  # Preenche com a cor branca
+        window.blit(background, (0, 0))
+        # atualiza a tela
+        pygame.display.update()
