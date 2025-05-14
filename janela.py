96c7b0e006baaad2c70017965960824023f8d051
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-14 14:48:09 -0300

    Janela Criada

---
+++ janela.py
@@ -0,0 +1,21 @@
+import pygame
+from configuracoes import *
+clock = pygame.time.Clock()
+def janela(window):
+    # ----- Gera tela principal
+    window = pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption('Py Racing')
+    pygame.display.update()
+    running = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False
+        window.fill(BLACK)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip
+
+    return state
