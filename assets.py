7da812364c00db13bb2e53241eed202015ee7afa
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-15 09:04:02 -0300

    Colocada um sprite pro carro (temporário)

--- assets.py
+++ assets.py
@@ -1,5 +1,9 @@
 import pygame
 from configuracoes import *
 fundo = 'fundo'
-assets = {}
-assets[fundo] = pygame.image.load('files/images/main_field.png')
+
+def load_assets():
+    assets = {}
+    assets[fundo] = pygame.image.load('files/images/main_field.png').convert()
+    assets['Carro_Piloto'] = pygame.image.load('files/images/piloto.png').convert_alpha()
+    return assets
