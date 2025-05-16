7f24b93d97afa48a3421f024696f4fd9d0936959
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-16 11:17:44 -0300

    Introduzindo a animação

--- assets.py
+++ assets.py
@@ -1,10 +1,19 @@
 import pygame
 from configuracoes import *
+import os
 fundo = 'fundo'
-
+explosao = 'explosao'
 def load_assets():
     assets = {}
-    assets[fundo] = pygame.transform.scale(pygame.image.load('files/images/main_field.png').convert(),(WIDTH,HEIGHT))
-    assets['Carro_Piloto'] = pygame.transform.scale( pygame.image.load('files/images/piloto.png').convert_alpha(), (WIDTH_CAR,HEIGHT_CAR))
-    assets['inimigo'] = pygame.transform.scale( pygame.image.load('files/images/inimigo.png').convert_alpha(), (WIDTH_CAR,HEIGHT_CAR))
+    assets[fundo] = pygame.transform.scale(pygame.image.load('files/img/main_field.png').convert(),(WIDTH,HEIGHT))
+    assets['Carro_Piloto'] = pygame.transform.scale( pygame.image.load('files/img/piloto.png').convert_alpha(), (WIDTH_PILOT,HEIGHT_PILOT))
+    assets['inimigo'] = pygame.transform.scale( pygame.image.load('files/img/inimigo.png').convert_alpha(), (WIDTH_CAR,HEIGHT_CAR))
+    explosion_anim = []
+    for i in range(9):
+        # Os arquivos de animação são numerados de 00 a 08
+        filename = os.path.join(IMG_DIR, 'pixel_art_esplosion_0{}.png'.format(i))
+        img = pygame.image.load(filename).convert()
+        img = pygame.transform.scale(img, (32, 32))
+        explosion_anim.append(img)
+        assets[explosao] = explosion_anim
     return assets
