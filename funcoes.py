e768cef7e6413ec3f82c861fb804c5fb719153ab
Luis Salvatore <luiseduardosalvatore@gmail.com>
2025-05-26 17:17:15 -0300

    atalizado

--- funcoes.py
+++ funcoes.py
@@ -5,7 +5,7 @@ from classes import *
 from assets import load_assets
 
 def fade(janela, atual, proximo, pos, duracao, passado):
-    alpha = min(255, max(0, 255 * (passado / duracao)))  # Clamp alpha to 0–255
+    alpha = min(255, max(0, 255 * (passado / duracao))) 
     atual.set_alpha(255 - alpha)
     proximo.set_alpha(alpha)
     janela.blit(atual, pos)
